89c34f4adc58dfdda71bae380d9fa7ce1f40b4be
Taylor <tayjed2010@gmail.com>
2018-07-06 11:48:19 -0600

    added port

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const data = require('./data/cohorts')
+const port = process.env.PORT || 3000
 
 function findById(data, id){
     for (let i = 0; i < data.length; i++){
@@ -32,4 +33,4 @@ app.get("/:id", function (req, res) {
 })
 
 
-app.listen(3000)
+app.listen(port)
