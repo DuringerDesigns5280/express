00b8cd8b543766374f7753a3fde1b03bb72e2cef
Taylor <tayjed2010@gmail.com>
2018-07-06 11:30:27 -0600

    init commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require('cors')
+const data = require('./data/cohorts')
+
+function findById(data, id){
+    for (let i = 0; i < data.length; i++){
+        if(data[i].id == id){
+            return data[i]
+        }
+    }
+    return null
+}
+
+const app = express()
+app.use(cors())
+
+app.get("/", function (request, response) {
+    response.json({data})
+})
+
+app.get("/:id", function (req, res) {
+    var rec = findById(data, req.params.id)
+    if(!rec){
+        res.status(404)
+        res.json({
+            error: {
+                message: "No Record Found"
+            }
+        })
+    }
+    res.json({data: rec})
+})
+
+
+app.listen(3000)
